e063de9713219732a17c81b74fe84aef6fe903fd
unknown <saketh.add@gmail.com>
2024-08-10 12:19:32 +0530

    firebase creds

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,19 +1,31 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+
+// const firebaseConfig = {
+// 	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+// 	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+// 	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+// 	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+// 	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+// 	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+// };
 
 const firebaseConfig = {
-	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-	authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+  apiKey: "AIzaSyDiGcM2FnDRP98JYEIMtnWfwVQKwRGoyzQ",
+  authDomain: "leetcode-clone-6d8f4.firebaseapp.com",
+  projectId: "leetcode-clone-6d8f4",
+  storageBucket: "leetcode-clone-6d8f4.appspot.com",
+  messagingSenderId: "403682839738",
+  appId: "1:403682839738:web:76c773ca5e80aa2795c683",
+  measurementId: "G-Y65T0C4G1F"
+}
 
 const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
 
 const auth = getAuth(app);
 const firestore = getFirestore(app);
+const analytics = getAnalytics(app);
 
 export { auth, firestore, app };
