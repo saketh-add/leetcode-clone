c4ba832e96d3dc1dde98e47af0b9d8f45f737f2d
unknown <saketh.add@gmail.com>
2024-08-10 12:23:07 +0530

    firebase chnages

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,7 +1,6 @@
 import { initializeApp, getApp, getApps } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
 
 // const firebaseConfig = {
 // 	apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -26,6 +25,5 @@ const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
 
 const auth = getAuth(app);
 const firestore = getFirestore(app);
-const analytics = getAnalytics(app);
 
 export { auth, firestore, app };
